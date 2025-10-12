6a9b55df0687238b403846e77e9b407883eb28dc
Kristi930 <kristidahal51@gmail.com>
2025-10-12 21:29:20 +0800

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,3 @@
-import express from 'express';
-
-var router = express.Router();
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'World' });
+  res.send('My Student ID is 21932252. Welcome to Express');
 });
-
-export default router;
